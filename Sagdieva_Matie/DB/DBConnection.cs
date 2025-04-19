6dca90a6baec481a9ac60bfc51ec47ec2cc420ac
Camillak2 <ksagdieva2@gmail.com>
2025-04-19 08:46:30 +0300

    Добавьте файлы проекта.

---
+++ Sagdieva_Matie/DB/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Sagdieva_Matie.DB
+{
+    internal class DBConnection
+    {
+        public static SagdievaK_Matie matie = new SagdievaK_Matie();
+
+        public static User logginedUser;
+    }
+}
---
+++ Sagdieva_Matie/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sagdieva_Matie
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SvernutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+            "Свернуть окно?",
+            "Подтверждение",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Window window = Window.GetWindow(this);
+                if (window != null)
+                {
+                    window.WindowState = WindowState.Minimized;
+                }
+            }
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show(
+            "Закрыть приложение?",
+            "Подтверждение закрытия",
+            MessageBoxButton.YesNo,
+            MessageBoxImage.Question);
+
+
+            if (result == MessageBoxResult.Yes)
+            {
+                Window.GetWindow(this)?.Close();
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
